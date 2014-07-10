07177ad1d91f047cd3c05d3c27362555ce542f45
Josh Rincon <memonkey@gmail.com>
2014-07-10 10:46:52 -0700

    +entries page +gitignore

---
+++ src/com/joshrincon/test/EntryServlet.java
@@ -0,0 +1,34 @@
+package com.joshrincon.test;
+
+import com.joshrincon.beans.SingletonEntriesFeed;
+import com.rometools.rome.feed.synd.SyndEntry;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by on 7/10/2014.
+ */
+public class EntryServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int entryPosition = Integer.parseInt(request.getParameter("action"));
+
+        SyndEntry entry = SingletonEntriesFeed.getInstance().getAnEntry(entryPosition);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("entry", entry);
+
+        request.getRequestDispatcher("/WEB-INF/jsps/entry.jsp").forward(request, response);
+
+
+    }
+}
